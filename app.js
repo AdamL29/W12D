0a78352ed22a97ff95402c99fb241914feeaab4f
Adam <adamlederer@live.ca>
2022-11-28 21:58:39 -0700

    finished what I could for now. Will return for fix up

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ function login(){
         url: "https://reqres.in/api/login",
         method: "POST",
         data:{
-            email: document.getElementById(`usernameBox`).value,
-            password: document.getElementById(`passwordBox`).value
+            email: document.getElementById(`emailBox`),
+            password: document.getElementById(`passwordBox`)
         }
-    }).then(success).catch(fail);
+    }).then(loginSuccess).catch(fail);
 }
 
-function success(response){
+function colours(){
+    axios.request({
+        url: "https://reqres.in/api/unknown",
+        method: "GET",
+    }).then(colourSuccess).catch(fail);
+}
+
+function colourSuccess(response){
+    let colourData = response.data
+    document.getElementById(`colourHolder`).insertAdjacentHTML(`beforeend`, `<p>${colourData.colorname}</p>`)
+}
+
+function loginSuccess(response){
     Cookies.set(`sessionToken`, response.data.token);
-    location.href = `battle.html`;
+    location.href = `home.html`;
+    alert(`it worked`)
 }
 
 function fail(error){
@@ -19,5 +32,6 @@ function fail(error){
     alert(`Your Username or Password is invalid`)
 }
 
+document.getElementById(`colourButton`).addEventListener(`click`, colours);
 document.getElementById(`loginButton`).addEventListener(`click`, login);
 // you can put a second api call in the success function of the first api call as a chain.
